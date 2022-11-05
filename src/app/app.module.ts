47995bb8d512ebad84ab6cff46547a451e7b3943
Amal Ben yakhlef <amel.benyakhlef@gmail.com>
2022-11-05 17:55:22 +0100

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultModule } from './layouts/default/default.module';
+/* import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component'; */
+/* import { MatButtonModule } from '@angular/material/button'; // it's a module to add a button in the html file !! */
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent /* DefaultComponent, DashboardComponent */],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DefaultModule    //upload the default module to see the new  component
+    /* MatButtonModule, */
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
